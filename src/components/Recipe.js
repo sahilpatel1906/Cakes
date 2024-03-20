a1bf1c9b5c30da1e29bf28f5a956e87f4f3a7d82
Sahil Patel <sahilpatel1906@gmail.com>
2024-03-20 10:45:24 -0500

    completed MVP

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,8 +1,18 @@
-const Recipe = () => {
-    return ( 
-        <>
+const Recipe = ({cakeName, cakeIngredients, cakePrice, cakeRating}) => {
 
-        </>
+    const IngredientsListElements = cakeIngredients.map((ingredient, index) => <li key={index}>{ingredient}</li>)
+
+    return ( 
+        <div>
+            <h3>{cakeName}</h3>
+            <section>
+            <h4>Ingredients:</h4>
+            <ul>{IngredientsListElements}</ul>
+
+            </section>
+            <h4>Price: {cakePrice}</h4>
+            <h4>Rating: {cakeRating}</h4>
+        </div>
     );
 }
  
